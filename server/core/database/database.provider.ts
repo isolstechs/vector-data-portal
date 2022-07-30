677a01e81f3d1eb337fcc75fa5f8cec9396a92b1
Rahmat Ali <RahmatAliMalik5@gmail.com>
2022-07-30 22:42:18 +0500

    updated force

--- server/core/database/database.provider.ts
+++ server/core/database/database.provider.ts
@@ -7,17 +7,23 @@ import { OperatorModel } from './model/operator.model';
 import { CallRecordModel } from './model/call-record.model';
 import { PrefixModel } from './model/prefix.model';
 
-let force = false; // <----------------- To Initialize Database
+let force = true; // <----------------- To Initialize Database
 let alter = false; // <----------------- To Initialize Database
 
-force =
-  typeof process.env['PROD_DB_FORCE'] == 'string'
-    ? process.env['PROD_DB_FORCE'] == 'true'
-    : force;
-alter =
-  typeof process.env['PROD_DB_ALTER'] == 'string'
-    ? process.env['PROD_DB_ALTER'] == 'true'
-    : alter;
+// force =
+//   typeof process.env['PROD_DB_FORCE'] == 'string'
+//     ? process.env['PROD_DB_FORCE'] == 'true'
+//     : force;
+// alter =
+//   typeof process.env['PROD_DB_ALTER'] == 'string'
+//     ? process.env['PROD_DB_ALTER'] == 'true'
+//     : alter;
+
+// for production we have made force to false
+if (process.env['NODE_ENV'] == 'production') {
+  force = false;
+}
+
 export const databaseProvider = [
   {
     provide: SEQUELIZE,
@@ -43,16 +49,16 @@ export const databaseProvider = [
       ]);
 
       await sequelize.sync({ force, alter }).then(async () => {
-        if (force) {
-          await seed();
-          setTimeout(() => {
-            console.log('database created');
-          }, 500);
-        } else if (alter) {
-          setTimeout(() => {
-            console.log('\nDB updated\n');
-          }, 500);
-        }
+        // if (force) {
+        //   await seed();
+        //   setTimeout(() => {
+        //     console.log('database created');
+        //   }, 500);
+        // } else if (alter) {
+        //   setTimeout(() => {
+        //     console.log('\nDB updated\n');
+        //   }, 500);
+        // }
       });
       return sequelize;
     },
